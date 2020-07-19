7a396c5e40ae317ec81efba2eec81ec0b1297cd1
olive <oliveira.v653>
2020-07-18 22:16:40 -0300

    seção11 m: tratamento e mipulação de datas

---
+++ src/TimeHandler/sumDatas.java
@@ -0,0 +1,24 @@
+package TimeHandler;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+
+public class sumDatas {
+    public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        Date d = Date.from(Instant.parse("2020-07-18T22:00:00Z"));
+
+        System.out.println(sdf.format(d));
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(d);
+        cal.add(Calendar.HOUR_OF_DAY, 4);
+        d = cal.getTime();
+
+        System.out.println(sdf.format(d));
+
+    }
+}
